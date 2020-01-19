89c0b5090b514782435e6872077cc27b156da437
hycnsz <1958006691@qq.com>
2020-01-19 14:20:36 +0800

    商品模型

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -13,8 +13,8 @@ class HomeController extends Controller
     public function index(Content $content)
     {
         return $content
-            ->title('Dashboard')
-            ->description('Description...')
+            ->title('首页')
+            ->description('仪表板')
             ->row(Dashboard::title())
             ->row(function (Row $row) {
 

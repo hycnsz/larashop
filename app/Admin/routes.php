18f9068b512714050df03d687b9b293032a83152
hycnsz <1958006691@qq.com>
2020-01-19 14:41:44 +0800

    管理后台 - 商品列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,4 +12,5 @@ Route::group([
 
     $router->get('/', 'HomeController@index')->name('admin.home');
     $router->get('/users','UsersController@index')->name('admin.users');
+    $router->get('products', 'ProductsController@index');
 });
